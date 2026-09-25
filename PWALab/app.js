bdfeebc5827bf532335ca186f805bbd6f6413cf0
UR Rehman <umair.rehman221@gmail.com>
2026-09-25 16:25:14 +0400

    Implement service worker registration
    Added service worker registration for PWA support.

--- PWALab/app.js
+++ PWALab/app.js
@@ -8,3 +8,15 @@ button.addEventListener("click", async () => {
 
     output.textContent = JSON.stringify(data, null, 2);
 })
+
+if("serviceWorker" in navigator){
+    navigator.serviceWorker.register("./sw.js")  // our first step towards pwa
+    .then((registration) => {                    // promise 
+        console.log("SerWkr registered:", registration);
+        console.log("Controller:", navigator.serviceWorker.controller); // service worker becomes the one who lead. not browser
+    })
+    .catch((error) => {
+        console.error("SW registration failed:", error);
+    });
+}
+
